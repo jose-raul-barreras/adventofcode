a2a2e83b8a274b5aa7107d20fd7dc02520157b65
Raul Barreras <10443833+jose-raul-barreras@users.noreply.github.com>
2024-12-09 12:35:40 -0600

    part 2 solved

--- 2024/src/d08_resonant_collinearity.py
+++ 2024/src/d08_resonant_collinearity.py
@@ -3,7 +3,8 @@
 
 import os
 import unittest
-import math 
+import itertools
+import math
 
 # https://adventofcode.com/2024/day/8
 
@@ -55,16 +56,29 @@ def antinodes(x, y):
     dx = p2[0] - p1[0]
     dy = p2[1] - p1[1]
 
-    if dy/dx > 0:
+    # calculate the antinodes
+    # p3 and p4 are the antinodes of the line defined by p1 and p2
+
+    if dx == 0:
+        # vertical line case (dx = 0)
+        p3 = (-1, -1)
+        p4 = (-1, -1)
+    elif dy/dx > 0:
         p3 = (p1[0] - dx, p1[1] - dy)
         p4 = (p2[0] + dx, p2[1] + dy)
     elif dy/dx < 0:
         p3 = (p1[0] - dx, p1[1] - dy)
         p4 = (p2[0] + dx, p2[1] + dy)
     else:
-        raise ValueError("Invalid slope")
+        # vertical line case (dy = 0)
+        p3 = (-1, -1)
+        p4 = (-1, -1)
         
     return p3, p4
+
+def remove_out_of_bounds(lst, grid_size):
+    # remove out of bounds antinodes and duplicates
+    return sorted({element for element in lst if 0 <= element[0] < grid_size[0] and 0 <= element[1] < grid_size[1]})
 
 def get_all_antinodes(grid, grid_size):
     antinodes_list = []
@@ -72,14 +86,56 @@ def get_all_antinodes(grid, grid_size):
         if key in [".", "#"]:
             continue
         if len(grid[key]) > 1:
-            # print(key, ":", grid[key])
             for i in range(len(grid[key])-1):
               for j in range(i+1, len(grid[key])):
                 for antinode in antinodes(grid[key][i], grid[key][j]):
                     antinodes_list.append(antinode)
 
-    return sorted({ antinode for antinode in antinodes_list if 0 <= antinode[0] < grid_size[0] and 0 <= antinode[1] < grid_size[1]})
+    return remove_out_of_bounds(antinodes_list, grid_size)
 
+### Harmonic antinodes ###
+
+def harmonic_antinodes(p1, p2, grid_size):
+    # return all the points in the line defined by p1 and p2
+    # such that all the points are within the grid
+    # the distance between the points pn and pn+1 is the same as the distance between p1 and p2
+    points = []
+    dx = p2[0] - p1[0]
+    dy = p2[1] - p1[1]
+    gcd = abs(dx) if dy == 0 else abs(dy) if dx == 0 else abs(math.gcd(dx, dy))
+    step_x = dx // gcd
+    step_y = dy // gcd
+
+    for i in range(-grid_size[0], grid_size[0]):
+        x = p1[0] + i * step_x
+        y = p1[1] + i * step_y
+        if 0 <= x < grid_size[0] and 0 <= y < grid_size[1]:
+            points.append((x, y))
+
+    return points
+
+def get_antinodes_for_frequency(grid, grid_size, frequency):
+    antinodes_list = []
+    antennas_pairs = list(itertools.combinations(grid[frequency], 2))
+    for antennas in antennas_pairs:
+        p1 = min(antennas)
+        p2 = max(antennas)
+        antinodes_list = antinodes_list + harmonic_antinodes(p1, p2, grid_size)
+
+    return remove_out_of_bounds(antinodes_list, grid_size)
+
+def get_harmonic_antinodes(grid, grid_size):
+    antinodes_list = set()
+    for key in grid.keys():
+        if key in [".", "#"]:
+            continue
+        if len(grid[key]) > 1:
+            antinodes_list_key = set(grid[key])
+            antinodes_list_key = antinodes_list.union(get_antinodes_for_frequency(grid, grid_size, key))
+            antinodes_list = antinodes_list.union(antinodes_list_key)
+
+    res = remove_out_of_bounds(antinodes_list, grid_size)
+    return res
 
 ### Unit tests ###
 
@@ -98,6 +154,25 @@ class TestAdventOfCodeDay(unittest.TestCase):
         # grid['#'] = antinodes_list
         # print_grid(grid, grid_size)
 
+    def test_harmonic_antinodes(self):
+        grid, grid_size = load_data("test_08_T.txt")
+        print_grid(grid, grid_size)
+        antinodes_list = get_harmonic_antinodes(grid, grid_size)
+        grid["#"] = antinodes_list
+        print_grid(grid, grid_size)
+        self.assertEqual(len(antinodes_list), 9)
+        print(grid["#"])
+
+        grid, grid_size = load_data("test_08.txt")
+        print_grid(grid, grid_size)
+        antinodes_list = get_harmonic_antinodes(grid, grid_size)
+        grid["#"] = antinodes_list
+        print_grid(grid, grid_size)
+        self.assertEqual(len(antinodes_list), 34)
+        print(grid["#"])
+       
+
+
 # main function
 
 
@@ -108,6 +183,9 @@ def main():
     antinodes_list = get_all_antinodes(grid, grid_size)
     print(len(antinodes_list))
     
+    antinodes_list = get_harmonic_antinodes(grid, grid_size)
+    print(len(antinodes_list))
+
 
 if __name__ == "__main__":
     import sys 
